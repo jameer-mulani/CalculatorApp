9ef442ca5c496ea32544e12867c4c4c2fa34650f
jameer <jameer.a.mulani@gmail.com>
2015-06-26 12:47:40 +0530

    checked

--- app/src/main/java/com/example/jameer/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/jameer/calculatorapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.jameer.calculatorapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +17,7 @@ public class MainActivity extends ActionBarActivity {
         {
             Log.d("test","first time created");
         }else{
-            Log.d("test","recreated instance");
+            Log.d("test","recreated instance"); 
         }
     }
 
