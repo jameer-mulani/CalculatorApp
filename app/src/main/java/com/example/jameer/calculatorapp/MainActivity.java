73420c26419b1c7fc6b75546c955ec11f96feccb
jameer <jameer.a.mulani@gmail.com>
2015-06-26 12:28:34 +0530

    added dummy check

--- app/src/main/java/com/example/jameer/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/jameer/calculatorapp/MainActivity.java
@@ -12,6 +12,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null)
+        {
+
+        }else{
+
+        }
     }
 
     @Override
